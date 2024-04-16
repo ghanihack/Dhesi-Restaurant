fa1ea54266dc08e65b33cff64f9226d9743eb4f6
ghanihack <166715271+ghanihack@users.noreply.github.com>
2024-04-16 19:33:36 +0530

    Update otp.js

--- otp.js
+++ otp.js
@@ -4,24 +4,35 @@ function getCodeBoxElement(index) {
 
 function onKeyUpEvent(index, event) {
   const eventCode = event.which || event.keyCode;
-  if (getCodeBoxElement(index).value.length === 1) {
+  const maxLength = 1; // Set the maximum length of each input box to 1
+  const currentElement = getCodeBoxElement(index);
+
+  // Check if the input box is filled
+  if (currentElement.value.length === maxLength) {
     if (index !== 5) {
+      // Move focus to the next input box
       getCodeBoxElement(index + 1).focus();
     } else {
-      getCodeBoxElement(index).blur();
+      // Last input box is filled, so blur it
+      currentElement.blur();
 
-      console.log("submit code ");
+      // Perform action after all OTP digits are entered (e.g., submit code)
+      console.log("Submit code");
     }
   }
-  if (eventCode === 5 && index !== 1) {
+
+  // Handle backspace key
+  if (eventCode === 8 && index !== 1) {
+    // Move focus to the previous input box when backspace is pressed
     getCodeBoxElement(index - 1).focus();
   }
 }
 
 function onFocusEvent(index) {
-  for (item = 1; item < index; item++) {
+  for (let item = 1; item < index; item++) {
     const currentElement = getCodeBoxElement(item);
     if (!currentElement.value) {
+      // Focus on the first empty input box encountered
       currentElement.focus();
       break;
     }
